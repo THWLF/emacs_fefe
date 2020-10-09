3bd19ca62852b5c5123a8b96a9881b262d211ca6
THWLF <games9me@web.de>
2020-10-09 23:04:55 +0200

    Initial commit

---
+++ fefe.el
@@ -0,0 +1,37 @@
+;;; fefe.el -- View fefe Blog from emacs
+(require 'eww)
+
+;Loads Today Fefe posts
+(defun fefe-today ()
+  (interactive)	
+  (eww "https://blog.fefe.de/"))
+
+
+;Loads Current Months Fefe posts
+(defun fefe-current-month ()
+ (interactive)
+ (eww  (concat "https://blog.fefe.de/?mon=" (format-time-string "%Y%m")))
+ )
+
+;Loads Selected Months
+
+;Loads Selected Day
+
+
+;Search For a special Kayword
+(defun fefe-search ()
+  (interactive)
+  (eww  (concat "https://blog.fefe.de/?q=" (read-string "Enter Keyword(Case Sensitive):")))
+)
+
+
+
+
+;  (defun calendar-insert-date ()
+;  "Capture the date at point, exit the Calendar, insert the date."
+;  (interactive)
+;  (seq-let (month day year) (save-match-data (calendar-cursor-to-date))
+;    (calendar-exit)
+;   (insert (format "%d-%02d-%02d" year month day))))
+;
+;(define-key calendar-mode-map (kbd "RET") 'calendar-insert-date)
